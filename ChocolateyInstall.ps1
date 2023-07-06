60b723aaba15b31c38e7f50341887aa85a19aaa8
NazarServer322 <66203891+NazarServer322@users.noreply.github.com>
2023-07-06 12:39:21 +0300

    Add files via upload

---
+++ ChocolateyInstall.ps1
@@ -0,0 +1,8 @@
+
+if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+else {
+    Write-Host "Chocolatey is already installed."
+}
